a8368ac4e4910186b8f916ebc2555b432ea224ca
gyulchyoung <hacha2011@naver.com>
2022-12-14 16:12:40 +0900

    [feat] add

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -151,3 +151,88 @@ def predict():
     result = ''.join(map(str, resultArr))
 
     return jsonify({"result": result})
+
+
+@bp.route('/imageCompare', methods=['POST'])
+def predict():
+    res = false;
+    file = request.files['haveImage']
+    file.save('C:/projects/myproject/pybo/uploads/' + secure_filename(file.filename))
+
+    fileRoutes = request.form.get('fileRoutes')
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+    # 원본 사진 불러와서 resizing
+    path1 ='C:/projects/myproject/pybo/uploads/' + file.filename
+
+    for path2 in fileRoutes:
+        if check_accuracy(path1, path2):
+            res = true
+            return jsonify({"result": res})
+
+
+
+
+def check_accuracy(path2, path1):
+
+    # print(path1 +path2)
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+    # cv2.imshow('img',img2)
+
+    h1, w1, c = img1.shape
+    h2, w2, c = img2.shape
+
+    if (h1 > h2/5) and h1 != h2 and w1!=w2:
+        img1 = cv2.resize(img1, (h2,w2))
+
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    # ORB, BF-Hamming 로 knnMatch  ---①
+    detector = cv2.ORB_create()
+    kp1, desc1 = detector.detectAndCompute(gray1, None)
+    kp2, desc2 = detector.detectAndCompute(gray2, None)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = matcher.match(desc1, desc2)
+
+    # 매칭 결과를 거리기준 오름차순으로 정렬 ---③
+    matches = sorted(matches, key=lambda x:x.distance)
+    # 모든 매칭점 그리기 ---④
+    res1 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
+                    flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+    # cv2.imshow('Matching-All', res1)
+    # cv2.waitKey()
+
+    # 매칭점으로 원근 변환 및 영역 표시 ---⑤
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ])
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ])
+    # RANSAC으로 변환 행렬 근사 계산 ---⑥
+    mtrx, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    h,w = img1.shape[:2]
+    pts = np.float32([ [[0,0]],[[0,h-1]],[[w-1,h-1]],[[w-1,0]] ])
+    dst = cv2.perspectiveTransform(pts,mtrx)
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    # 정상치 매칭만 그리기 ---⑦
+    matchesMask = mask.ravel().tolist()
+    res2 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
+                        matchesMask = matchesMask,
+                        flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    # 모든 매칭점과 정상치 비율 ---⑧
+    accuracy=float(mask.sum()) / mask.size
+    print("match : %d"% (mask.sum()))
+
+    # 결과 출력
+    cv2.imshow('Matching-All', res1)
+    cv2.imshow('Matching-Inlier ', res2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    if mask.sum()>100:
+        return True
+    else:
+        return False
